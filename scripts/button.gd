120575ede82418576f72e6087f01f6ed3b299db1
diegosoriarios <diegosoriarios@gmail.com>
2020-02-25 22:46:29 -0300

    first commit

---
+++ scripts/button.gd
@@ -0,0 +1,11 @@
+extends TextureButton
+
+signal was_pressed
+
+func _pressed():
+	emit_signal("was_pressed")
+
+func connect_me(obj, unit_name):
+	connect("was_pressed", obj, "was_pressed", [self])
+	name = unit_name
+	$Label.text = unit_name
