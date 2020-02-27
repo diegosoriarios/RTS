9514c5cb0cc16ffdc04138c46efd91c90f2e414e
diegosoriarios <diegosoriarios@gmail.com>
2020-02-27 00:01:43 -0300

    Enemy Shoot

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -2,13 +2,20 @@ extends KinematicBody2D
 
 var speed = 100
 
+var pos2d
 var player
 var dir
 var timer
 var turn = true
 var rotationValue = 1
+var can_shoot = true
+
+onready var Bullet = preload("res://scenes/bullet.tscn")
 
 func _ready():
+	pos2d = Position2D.new()
+	pos2d.position = self.position
+	get_parent().add_child(pos2d)
 	pass
 
 func _physics_process(delta):
@@ -21,9 +28,24 @@ func _physics_process(delta):
 			self.rotation -= aim_speed
 		dir = (player.global_position - global_position).normalized()
 		move_and_collide(dir * speed * delta)
+		if can_shoot:
+			shoot(player.position)
 	else:
 		#start_watch()
+		dir = (pos2d.global_position - global_position).normalized()
+		move_and_collide(dir * speed * delta)
 		pass
+
+func shoot(pos):
+	var b = Bullet.instance()
+	var a = (pos - global_position).angle()
+	b.start(global_position, a + rand_range(-0.05, 0.05))
+	get_parent().add_child(b)
+	can_shoot = false
+	$ShootTimer.start()
+
+func _on_ShootTimer_timeout():
+	can_shoot = true
 
 func start_watch():
 	self.rotation = deg2rad(rotationValue)
@@ -51,4 +73,4 @@ func start_timer():
 	timer.start()
 
 func _on_timer_timeout():
-   player = null
+	player = null
